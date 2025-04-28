0dd28d648c049cbfb2d786c752c6a61cf9b649a3
wooing26 <104512612+wooing26@users.noreply.github.com>
2025-04-28 13:18:14 +0900

    미니맵 쿼터뷰에서 회전 막기

--- Assets/02. Scripts/Camera/MinimapCamera.cs
+++ Assets/02. Scripts/Camera/MinimapCamera.cs
@@ -8,10 +8,16 @@ public class MinimapCamera : MonoBehaviour
     private void LateUpdate()
     {
         transform.position = Target.position + Vector3.up * YOffset;
-
+        
         Vector3 newEulerAngles = Target.eulerAngles;
         newEulerAngles.x = 90;
         newEulerAngles.z = 0;
+
+        if (CameraManager.Instance.CameraMode == CameraMode.QuarterView)
+        {
+            newEulerAngles.y = 0;
+        }
+
         transform.eulerAngles = newEulerAngles;
     }
 }
